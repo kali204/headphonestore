bdd4f2b4120324607ad9f6c7852ee1e3c7f731c7
kali204 <abhaystark77@gmail.com>
2025-07-28 17:58:09 +0530

    backend changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 Add this line to fix relative path issues
   plugins: [react()],
   optimizeDeps: {
     include: ['lucide-react'],
